330f401221e7ea230e8dbc8555e444be94319611
jvictorr01 <103452608+jvictorr01@users.noreply.github.com>
2022-09-13 10:54:54 -0300

    Add files via upload

---
+++ exercicio1.java
@@ -0,0 +1,20 @@
+package provapi;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class exercicio1  {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+        int numeros[] = new int[5];
+        for (int i =0; i < numeros.length; i++) {
+        	System.out.println("Escreva o " +(i+1)+ "º número: ");
+        	numeros[i] = ler.nextInt();
+        }
+        Arrays.sort(numeros);
+        for (int i=0; i < numeros.length; i++) {
+        	System.out.println("Números ["+i+"] = "+numeros[i]);
+        }
+        ler.close();
+	}
+}
